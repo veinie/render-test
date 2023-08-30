86bfca74eec26d83048d7d86bb4f7fe02472796c
veiniem <veikka.niemitukia@student.hamk.fi>
2023-08-30 18:36:29 +0300

    uibuild

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const requestLogger = (request, response, next) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('build'))
+app.use(express.static('dist'))
 
 
 let notes = [
